cc44da8f854a4bdef93a67b5d49644fb9a75fc25
Ramakrishna.Mbhaskar <Ramakrishna.Mbhaskar@MLBMYSL-107643>
2020-10-09 16:05:49 +0530

    array example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { element } from 'protractor';
+import { Sampleclass } from './sampleclass';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular10';
+   
+
+  array1: Sampleclass[] = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 11, name: 'Dr Nice' },
+    { id: 11, name: 'Dr Nice' },
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 12, name: 'Narco' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' },
+  ];
+
+  array2: Sampleclass[] = [
+   
+  ];
+
+  addNew() {
+    debugger;
+    //console.log(this.array1);
+
+    this.array1.forEach((ar1element) => {
+      if (!this.array2.some((ar2element) => ar2element.name === ar1element.name)) {
+        this.array2.push(ar1element);
+      }
+    });
+     console.log(this.array2);
+
+   
+  }
+ 
 }
---
+++ src/app/sampleclass.ts
@@ -0,0 +1,4 @@
+export interface Sampleclass {
+    id: number;
+    name: string;
+  }
